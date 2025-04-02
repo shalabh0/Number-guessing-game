d64908ed1582394d5395e01ffc5179382d0a0f46
Shalabh <shalabhaj99@gmail.com>
2025-04-02 17:07:32 +0530

    Initial commit

---
+++ Project2.cs
@@ -0,0 +1,68 @@
+using System;
+
+class Program2{
+    static int bestScore = int.MaxValue;
+    public static void Main(){
+        Console.WriteLine("Welcome to number guessing game !!");
+        Console.WriteLine("I am thinking of a number between 1 and 100.");
+
+
+        Random random = new Random();
+        int secretNumber = random.Next(1,101);
+    
+
+    int guess = 0;
+    int attempt = 0;
+    int macAttempt = 25;
+
+    while(guess != secretNumber && attempt < macAttempt)
+    {
+        Console.WriteLine("Enter your guess");
+        string input = Console.ReadLine();
+
+        if(int.TryParse(input, out guess))
+        {
+            attempt++;
+            if(Math.Abs(guess - secretNumber)>20)
+            {
+                Console.WriteLine("You are way off the answer");
+            }
+            else if(Math.Abs(guess - secretNumber)>10)
+            {
+                Console.WriteLine("You are getting closer to the answer");
+            }
+            else{
+                Console.WriteLine($"Guess is correct in the {attempt} attempt!!");
+                if(attempt < bestScore)
+               {
+                bestScore = attempt;
+                Console.WriteLine($"New best score :{bestScore} attempts");
+               }
+               break;
+            }
+            
+            
+
+        }
+        else{
+            Console.WriteLine("Invalid input !!");
+        }
+    }
+    if(attempt >= macAttempt && guess != secretNumber)
+    {
+        Console.WriteLine($"You have used  your max attempts and the correct number was {secretNumber}");
+    }
+    Console.WriteLine("Do you want to play again :(Yes/No) :");
+    string playAgain = Console.ReadLine().ToLower();
+
+    if(playAgain == "yes")
+    {
+        Main();
+    }
+    else
+    {
+
+     Console.WriteLine("Thanks for playing");
+    }
+} 
+}
